064eb366d4ebd0b6be048b505703b3087f9f5a4c
Alumno <Alumno@LAB1509-27>
2023-12-01 17:03:01 -0500

    Agregar archivos de proyecto.

---
+++ MVVMDemo/MVVMDemo/Model/TaskModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MVVMDemo.Model
+{
+    public class TaskModel
+    {
+      
+        public string Title { get; set; }
+        public string Description { get; set; }
+ 
+
+    public TaskModel() { }
+    }
+}
---
+++ MVVMDemo/MVVMDemo/ViewModel/TaskViewModel.cs
@@ -0,0 +1,75 @@
+﻿using MVVMDemo.Model;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Text;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace MVVMDemo.ViewModel
+{
+    public class TaskViewModel : ViewModelBase
+    {
+
+
+        string title;
+        public string Title
+        {
+            get { return title; }
+            set
+            {
+                if (title != value)
+                {
+                    title = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        ObservableCollection<TaskModel> tasks;
+        public ObservableCollection<TaskModel> Tasks
+        {
+            get { return tasks; }
+            set
+            {
+                if (tasks != value)
+                {
+                    tasks = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        private List<TaskModel> taskTemp;
+
+        ICommand Get { set; get; }
+        ICommand Save { set; get; }
+
+        public TaskViewModel()
+        {
+            taskTemp = new List<TaskModel>();
+
+            Save = new Command(x =>
+            {
+                taskTemp.Add(new TaskModel {
+                
+                Title=this.Title,
+                Description=this.title
+                });
+
+            });
+
+
+            Get = new Command(x =>
+            {
+                Tasks = new ObservableCollection<TaskModel>(taskTemp);
+
+            });
+        }
+
+
+    }
+
+
+}
